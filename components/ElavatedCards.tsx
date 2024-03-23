54b4042a472ffff5e2fa4e8dd76dfb62e54ec602
tnavneet8628@gamil.com <tnavneer8628@gmail.com>
2024-03-23 23:41:17 +0530

    Empty elevated card component created

---
+++ components/ElavatedCards.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+const ElavatedCards = () => {
+  return (
+    <View>
+      <Text>ElavatedCards</Text>
+    </View>
+  );
+};
+
+export default ElavatedCards;
